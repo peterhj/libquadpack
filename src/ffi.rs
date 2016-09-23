478a52f00688a1d6f6a49d9ffd22473047ef149b
Peter Jin <peterhaijin@gmail.com>
2016-09-23 11:45:14 -0700

    Allow `dqags` and `dqagi` to accept user data. Be careful about the order of the arguments; `dqagi` in particular broke when inserting the new argument inside the original argument list.

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,5 @@
 use libc::*;
+use std::f32;
 use std::f64;
 
 #[link(name = "gfortran")]
@@ -15,8 +16,22 @@ pub extern "C" fn xermsg_(library: *mut c_void, subroutine: *mut c_void, msg: *m
 }
 
 #[no_mangle]
-pub extern "C" fn d1mach_(code: *mut c_int) -> f64 {
+pub extern "C" fn r1mach_(code: *mut c_int) -> f32 {
   match unsafe { *code } {
+    1 => f32::MIN_POSITIVE,
+    2 => f32::MAX,
+    3 => 0.5 * f32::EPSILON,
+    4 => f32::EPSILON,
+    5 => f32::consts::LN_2 / f32::consts::LN_10,
+    _ => unreachable!(),
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn d1mach_(code: *mut c_int) -> f64 {
+  let c = unsafe { *code };
+  println!("DEBUG: d1mach: code: {}", c);
+  match c {
     1 => f64::MIN_POSITIVE,
     2 => f64::MAX,
     3 => 0.5 * f64::EPSILON,
@@ -35,6 +50,7 @@ extern "C" {
       epsabs: *mut f32,
       epsrel: *mut f32,
       result: *mut f32,
+      abserr: *mut f32,
       neval: *mut c_int,
       ier: *mut c_int,
       limit: *mut c_int,
@@ -44,12 +60,14 @@ extern "C" {
       work: *mut f32,
   );
   pub fn dqagi_(
-      f: extern "C" fn (x: *mut f64) -> f64,
+      //f: extern "C" fn (x: *mut f64) -> f64,
+      f: extern "C" fn (x: *mut f64, udata: *mut c_void) -> f64,
       bound: *mut f64,
       inf: *mut c_int,
       epsabs: *mut f64,
       epsrel: *mut f64,
       result: *mut f64,
+      abserr: *mut f64,
       neval: *mut c_int,
       ier: *mut c_int,
       limit: *mut c_int,
@@ -57,9 +75,11 @@ extern "C" {
       last: *mut c_int,
       iwork: *mut c_int,
       work: *mut f64,
+      udata: *mut c_void,
   );
   pub fn qags_(
-      f: extern "C" fn (x: *mut f32) -> f32,
+      f: extern "C" fn (x: *mut f32, udata: *mut c_void) -> f32,
+      udata: *mut c_void,
       a: *mut f32,
       b: *mut f32,
       epsabs: *mut f32,
@@ -75,7 +95,7 @@ extern "C" {
       work: *mut f32,
   );
   pub fn dqags_(
-      f: extern "C" fn (x: *mut f64) -> f64,
+      f: extern "C" fn (x: *mut f64, udata: *mut c_void) -> f64,
       a: *mut f64,
       b: *mut f64,
       epsabs: *mut f64,
@@ -89,21 +109,6 @@ extern "C" {
       last: *mut c_int,
       iwork: *mut c_int,
       work: *mut f64,
+      udata: *mut c_void,
   );
-  /*pub fn dqags_data_(
-      f: extern "C" fn (x: *mut f64, data: *mut c_void),
-      data: *mut c_void,
-      a: *mut f64,
-      b: *mut f64,
-      epsabs: *mut f64,
-      epsrel: *mut f64,
-      result: *mut f64,
-      neval: *mut c_int,
-      ier: *mut c_int,
-      limit: *mut c_int,
-      lenw: *mut c_int,
-      last: *mut c_int,
-      iwork: *mut c_int,
-      work: *mut f64,
-  );*/
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,16 @@
 
 extern crate libc;
 
+pub use self::QuadpackInterval::*;
+
 use ffi::*;
 
 use libc::*;
 //use std::cell::{RefCell};
 use std::fmt::{Debug};
-use std::mem::{transmute};
+use std::mem::{size_of, transmute};
 use std::num::{Zero};
+//use std::ptr::{null_mut};
 //use std::rc::{Rc};
 
 pub mod ffi;
@@ -29,7 +32,15 @@ pub struct QuadpackScratch<T> where T: Copy {
 }
 
 impl<T> QuadpackScratch<T> where T: Copy + Zero {
-  pub fn new(len: usize) -> QuadpackScratch<T> {
+  pub fn new(sz_bytes: usize) -> QuadpackScratch<T> {
+    let quantum = (size_of::<i32>() + 4 * size_of::<T>());
+    let max_len = (sz_bytes + quantum - 1) / quantum;
+    //println!("scratch: {}", max_len);
+    assert!(max_len >= 1);
+    QuadpackScratch::with_max_subdivs(max_len)
+  }
+
+  pub fn with_max_subdivs(len: usize) -> QuadpackScratch<T> {
     let mut iwork = Vec::with_capacity(len);
     for _ in 0 .. len {
       iwork.push(0);
@@ -53,25 +64,33 @@ pub struct QuadpackResult<T> where T: Copy + Debug {
   pub nsubdiv:  usize,
 }
 
-pub struct QuadpackIntegrand<T, Data> where T: Copy {
-  pub f:    extern "C" fn (x: *mut T) -> T,
-  //pub f:    extern "C" fn (x: *mut T, data: *mut c_void),
-  pub data: Data,
+#[derive(Clone, Copy, Debug)]
+pub enum QuadpackInterval<T> {
+  Bounded(T, T),
+  UpperInf(T),
+  LowerInf(T),
+  Infinite,
 }
 
-impl<Data> QuadpackIntegrand<f64, Data> {
-  pub fn integrate_qags(&mut self, mut a: f64, mut b: f64, mut epsabs: f64, mut epsrel: f64, scratch: &mut QuadpackScratch<f64>) -> Result<QuadpackResult<f64>, QuadpackError> {
-    let data: *mut c_void = unsafe { transmute(&mut self.data) };
-    let mut result: f64 = 0.0;
-    let mut abserr: f64 = 0.0;
+pub struct QuadpackIntegrand<T, U> where T: Copy {
+  pub f:    extern "C" fn (x: *mut T, udata: *mut c_void) -> T,
+  pub data: U,
+}
+
+impl<U> QuadpackIntegrand<f32, U> {
+  pub fn integrate_qags(&mut self, mut a: f32, mut b: f32, mut epsabs: f32, mut epsrel: f32, scratch: &mut QuadpackScratch<f32>) -> Result<QuadpackResult<f32>, QuadpackError> {
+    let udata: *mut c_void = unsafe { transmute(&mut self.data) };
+    let mut result: f32 = 0.0;
+    let mut abserr: f32 = 0.0;
     let mut neval: c_int = 0;
     let mut ier: c_int = 0;
     let mut limit: c_int = scratch.iwork.len() as _;
     let mut lenw: c_int = scratch.work.len() as _;
     let mut last: c_int = 0;
     assert!(lenw >= 4 * limit);
-    unsafe { dqags_(
+    unsafe { qags_(
         self.f,
+        udata,
         &mut a as *mut _,
         &mut b as *mut _,
         &mut epsabs as *mut _,
@@ -104,18 +123,235 @@ impl<Data> QuadpackIntegrand<f64, Data> {
   }
 }
 
-#[test]
-fn test_qags() {
-  let epsrel = 1.0e-6;
-  extern "C" fn integrand_f(x: *mut f64) -> f64 {
-    3.14
+impl<U> QuadpackIntegrand<f64, U> {
+  /*pub fn integrate_qags(&mut self, mut a: f64, mut b: f64, mut epsabs: f64, mut epsrel: f64, scratch: &mut QuadpackScratch<f64>) -> Result<QuadpackResult<f64>, QuadpackError> {
+    let udata: *mut c_void = unsafe { transmute(&mut self.data) };
+    let mut result: f64 = 0.0;
+    let mut abserr: f64 = 0.0;
+    let mut neval: c_int = 0;
+    let mut ier: c_int = 0;
+    let mut limit: c_int = scratch.iwork.len() as _;
+    let mut lenw: c_int = scratch.work.len() as _;
+    let mut last: c_int = 0;
+    assert!(lenw >= 4 * limit);
+    unsafe { dqags_(
+        self.f,
+        udata,
+        &mut a as *mut _,
+        &mut b as *mut _,
+        &mut epsabs as *mut _,
+        &mut epsrel as *mut _,
+        &mut result as *mut _,
+        &mut abserr as *mut _,
+        &mut neval as *mut _,
+        &mut ier as *mut _,
+        &mut limit as *mut _,
+        &mut lenw as *mut _,
+        &mut last as *mut _,
+        scratch.iwork.as_mut_ptr(),
+        scratch.work.as_mut_ptr(),
+    ) };
+    match ier {
+      0 => Ok(QuadpackResult{
+        value:      result,
+        abserr:     abserr,
+        neval:      neval as _,
+        nsubdiv:    last as _,
+      }),
+      1 => Err(QuadpackError::MaxSubdivisions),
+      2 => Err(QuadpackError::RoundoffError),
+      3 => Err(QuadpackError::BadIntegrand),
+      4 => Err(QuadpackError::NoConverge),
+      5 => Err(QuadpackError::Divergent),
+      6 => Err(QuadpackError::InvalidInput),
+      _ => unreachable!(),
+    }
+  }*/
+
+  /*pub fn integrate_qagi(&mut self, interval: QuadpackInterval<f64>, mut epsabs: f64, mut epsrel: f64, scratch: &mut QuadpackScratch<f64>) -> Result<QuadpackResult<f64>, QuadpackError> {
+    let udata: *mut c_void = unsafe { transmute(&mut self.data) };
+    /*let (mut bound, mut inf): (f64, c_int) = match interval {
+      Bounded(_, _) => unimplemented!(),
+      UpperInf(lo)  => (lo, 1),
+      LowerInf(hi)  => (hi, -1),
+      Infinite      => (0.0, 2),
+    };*/
+    let mut bound: f64 = 0.0;
+    let mut inf: c_int = 2;
+    let mut result: f64 = 0.0;
+    let mut abserr: f64 = 0.0;
+    let mut neval: c_int = 0;
+    let mut ier: c_int = 0;
+    let mut limit: c_int = scratch.iwork.len() as _;
+    let mut lenw: c_int = scratch.work.len() as _;
+    let mut last: c_int = 0;
+    assert!(lenw >= 4 * limit);
+    unsafe { dqagi_(
+        self.f,
+        udata,
+        &mut bound as *mut _,
+        &mut inf as *mut _,
+        &mut epsabs as *mut _,
+        &mut epsrel as *mut _,
+        &mut result as *mut _,
+        &mut abserr as *mut _,
+        &mut neval as *mut _,
+        &mut ier as *mut _,
+        &mut limit as *mut _,
+        &mut lenw as *mut _,
+        &mut last as *mut _,
+        scratch.iwork.as_mut_ptr(),
+        scratch.work.as_mut_ptr(),
+    ) };
+    match ier {
+      0 => Ok(QuadpackResult{
+        value:      result,
+        abserr:     abserr,
+        neval:      neval as _,
+        nsubdiv:    last as _,
+      }),
+      1 => Err(QuadpackError::MaxSubdivisions),
+      2 => Err(QuadpackError::RoundoffError),
+      3 => Err(QuadpackError::BadIntegrand),
+      4 => Err(QuadpackError::NoConverge),
+      5 => Err(QuadpackError::Divergent),
+      6 => Err(QuadpackError::InvalidInput),
+      _ => unreachable!(),
+    }
+  }*/
+
+  pub fn integrate(&mut self, mut interval: QuadpackInterval<f64>, mut epsabs: f64, mut epsrel: f64, scratch: &mut QuadpackScratch<f64>) -> Result<QuadpackResult<f64>, QuadpackError> {
+    let mut result: f64 = 0.0;
+    let mut abserr: f64 = 0.0;
+    let mut neval: c_int = 0;
+    let mut ier: c_int = 0;
+    let mut limit: c_int = scratch.iwork.len() as _;
+    let mut lenw: c_int = scratch.work.len() as _;
+    let mut last: c_int = 0;
+    assert!(lenw >= 4 * limit);
+    let udata: *mut c_void = unsafe { transmute(&mut self.data) };
+    match interval {
+      Bounded(mut a, mut b) => {
+        unsafe { dqags_(
+            self.f,
+            &mut a as *mut _,
+            &mut b as *mut _,
+            &mut epsabs as *mut _,
+            &mut epsrel as *mut _,
+            &mut result as *mut _,
+            &mut abserr as *mut _,
+            &mut neval as *mut _,
+            &mut ier as *mut _,
+            &mut limit as *mut _,
+            &mut lenw as *mut _,
+            &mut last as *mut _,
+            scratch.iwork.as_mut_ptr(),
+            scratch.work.as_mut_ptr(),
+            udata,
+        ) };
+      }
+      _ => {
+        let (mut bound, mut inf): (f64, c_int) = match interval {
+          Bounded(_, _) => unimplemented!(),
+          UpperInf(lo)  => (lo, 1),
+          LowerInf(hi)  => (hi, -1),
+          Infinite      => (0.0, 2),
+        };
+        unsafe { dqagi_(
+            self.f,
+            &mut bound as *mut _,
+            &mut inf as *mut _,
+            &mut epsabs as *mut _,
+            &mut epsrel as *mut _,
+            &mut result as *mut _,
+            &mut abserr as *mut _,
+            &mut neval as *mut _,
+            &mut ier as *mut _,
+            &mut limit as *mut _,
+            &mut lenw as *mut _,
+            &mut last as *mut _,
+            scratch.iwork.as_mut_ptr(),
+            scratch.work.as_mut_ptr(),
+            udata,
+        ) };
+      }
+    };
+    match ier {
+      0 => Ok(QuadpackResult{
+        value:      result,
+        abserr:     abserr,
+        neval:      neval as _,
+        nsubdiv:    last as _,
+      }),
+      1 => Err(QuadpackError::MaxSubdivisions),
+      2 => Err(QuadpackError::RoundoffError),
+      3 => Err(QuadpackError::BadIntegrand),
+      4 => Err(QuadpackError::NoConverge),
+      5 => Err(QuadpackError::Divergent),
+      6 => Err(QuadpackError::InvalidInput),
+      _ => unreachable!(),
+    }
   }
-  let mut integrand = QuadpackIntegrand{
-    f:    integrand_f,
-    data: (),
-  };
-  let mut scratch = QuadpackScratch::new(1024);
-  let res = integrand.integrate_qags(0.0, 1.0, 0.0, epsrel, &mut scratch);
-  //panic!("integral: {:?}", res);
-  assert!(((res.unwrap().value - 3.14) / 3.14).abs() < 2.0 * epsrel);
+}
+
+#[test]
+fn test_qags_qagi() {
+  {
+    let epsrel = 1.0e-5;
+    extern "C" fn integrand_f32(x: *mut f32, udata: *mut c_void) -> f32 {
+      let hello: &String = unsafe { transmute(udata) };
+      println!("f: {}", hello);
+      3.14
+    }
+    let mut integrand = QuadpackIntegrand{
+      f:    integrand_f32,
+      data: format!("Hello, world!"),
+    };
+    let mut scratch = QuadpackScratch::new(4096);
+    let res = integrand.integrate_qags(0.0, 1.0, 0.0, epsrel, &mut scratch);
+    println!("integral(32): {:?}", res);
+    assert!(((res.unwrap().value - 3.14) / 3.14).abs() < 2.0 * epsrel);
+  }
+
+  {
+    let epsrel = 1.0e-6;
+    extern "C" fn integrand_f64(x: *mut f64, udata: *mut c_void) -> f64 {
+      /*let hello: &String = unsafe { transmute(udata) };
+      println!("f: {}", hello);
+      3.14*/
+      let x = unsafe { *x };
+      let hello: &String = unsafe { transmute(udata) };
+      let y = (-x.abs()).exp();
+      println!("f: {} x: {:e}, y: {:e}", hello, x, y);
+      y
+    }
+    let mut integrand = QuadpackIntegrand{
+      f:    integrand_f64,
+      data: format!("Hello, world!"),
+    };
+    let mut scratch = QuadpackScratch::new(4096);
+    let res = integrand.integrate(Bounded(0.0, 1.0), 0.0, epsrel, &mut scratch);
+    println!("integral(64): {:?}", res);
+    //assert!(((res.unwrap().value - 3.14) / 3.14).abs() < 2.0 * epsrel);
+  }
+
+  {
+    let epsabs = 0.0;
+    let epsrel = 1.0e-6;
+    extern "C" fn integrand_f64(x: *mut f64, udata: *mut c_void) -> f64 {
+      let x = unsafe { *x };
+      let hello: &String = unsafe { transmute(udata) };
+      let y = (-x.abs()).exp();
+      println!("f: {} x: {:e}, y: {:e}", hello, x, y);
+      y
+    }
+    let mut integrand = QuadpackIntegrand{
+      f:    integrand_f64,
+      data: format!("Hello, world!"),
+    };
+    let mut scratch = QuadpackScratch::new(4096);
+    let res = integrand.integrate(Infinite, epsabs, epsrel, &mut scratch);
+    println!("integral(64): {:?}", res);
+    assert!(((res.unwrap().value - 2.0) / 2.0).abs() < 2.0 * epsrel);
+  }
 }
